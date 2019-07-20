e75f4c68c1adc0a7357d48c921658ca03748c72a
Baba Umar <whizmoney@yahoo.com>
2019-07-20 07:21:47 +0100

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,6 +1,5 @@
 <?php
 //connect to the database and get all columns
-require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
 $connect = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
